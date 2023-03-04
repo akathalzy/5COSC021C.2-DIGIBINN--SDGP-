4b5f456c5e7b7a57c9387390b53ba22ebd0a763d
Hasitha Wimalasooriya <57836866+techhasi@users.noreply.github.com>
2023-03-04 15:06:45 +0530

    backend changes

--- ReactApp-Digibinn/digibinn_app/src/scenes/Register/Register.jsx
+++ ReactApp-Digibinn/digibinn_app/src/scenes/Register/Register.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import Footer from '../../components/Footer'
 import Navbar from '../../components/Navbar'
 import {Link} from 'react-router-dom';
-import registerUser from '../../api';
 
 
 function Register() {
@@ -11,7 +10,7 @@ function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [userType, setUserType] = useState("");
+    const [userType, setUserType] = useState("smartDustbinUser");
     const [termsAgreed, setTermsAgreed] = useState(false);
   
     const handleNameChange = (e) => {
@@ -42,23 +41,19 @@ function Register() {
         setTermsAgreed(e.target.checked);
       };
     
-      const handleSubmit = async (e) => {
+      const handleSubmit = (e) => {
         e.preventDefault();
-        const user = {
-            name,
-            number,
-            email,
-            password,
-            userType,
-          };
-          try {
-            const response = await registerUser(user);
-            console.log(response.data);
-          } catch (error) {
-            console.error(error);
-          }
-        
-      };
+        fetch('/api/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password, number, userType })
+          })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+        };
 
 
   return (
@@ -191,7 +186,7 @@ function Register() {
                         <div className="flex items-center mt-4">
                             <button 
                                 type="submit"
-                                disabled={!name || !email || !password || !confirmPassword || !termsChecked || !number || !userType}
+                                
                                 className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
                                 Register
                             </button>

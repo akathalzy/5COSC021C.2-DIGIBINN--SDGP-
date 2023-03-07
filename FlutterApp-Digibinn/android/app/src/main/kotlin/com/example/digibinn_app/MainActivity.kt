eb23f34d8aa8ece42220c71edb9a728d6e3cb41c
akathalzy <thalal.20200165@iit.ac.lk>
2023-03-07 23:50:17 +0530

    Added Flutter Files

---
+++ FlutterApp-Digibinn/android/app/src/main/kotlin/com/example/digibinn_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.digibinn_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ FlutterApp-Digibinn/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:digibinn_app/register.dart';
+import 'package:flutter/material.dart';
+
+
+import 'login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}

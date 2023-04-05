f115fa5c2c5f27f9a805ed72f6aeabab10916915
Kemila <kemiladabare9@gmail.com>
2023-04-05 08:49:01 +0530

    location added

--- FlutterApp-Digibinn/lib/pages/LocateBin.dart
+++ FlutterApp-Digibinn/lib/pages/LocateBin.dart
@@ -1,32 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class BinLocation extends StatelessWidget {
-  final LatLng binLocation = LatLng(6.795790, 79.943533);
-
   @override
-  Widget build(BuildContext context) => Scaffold(
-    //drawer: NavigationDrawerWidget(),
-    appBar: AppBar(
-      title: Text('Locate Bin'),
-      centerTitle: true,
-      backgroundColor: Colors.green,
-    ),
-    body: GoogleMap(
-      initialCameraPosition: CameraPosition(
-        target: binLocation,
-        zoom: 15,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Locate Bin'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
       ),
-      markers: {
-        Marker(
-          markerId: MarkerId('bin'),
-          position: binLocation,
-          infoWindow: InfoWindow(
-            title: 'Bin Location',
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(
+          url: Uri.parse('https://goo.gl/maps/SXmJWBm1T5mB22qx8'),
+        ),
+        initialOptions: InAppWebViewGroupOptions(
+          crossPlatform: InAppWebViewOptions(
+            javaScriptEnabled: true,
           ),
         ),
-      },
-    ),
-  );
+      ),
+    );
+  }
 }
+
+
+
+
+ // body: GoogleMap(
+    //   initialCameraPosition: CameraPosition(
+    //     target: binLocation,
+    //     zoom: 15,
+    //   ),
+    //   markers: {
+    //     Marker(
+    //       markerId: MarkerId('bin'),
+    //       position: binLocation,
+    //       infoWindow: InfoWindow(
+    //         title: 'Bin Location',
+    //       ),
+    //     ),
+    //   },
+    // ),

ba622de2380dd5e57347fe9d05cc2da3ac67ce7f
Kemila <kemiladabare9@gmail.com>
2023-04-05 07:49:30 +0530

    minor changes

--- FlutterApp-Digibinn/lib/pages/Dashboard.dart
+++ FlutterApp-Digibinn/lib/pages/Dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class DashBoard extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) => Scaffold(
         //drawer: NavigationDrawerWidget(),
@@ -12,63 +13,62 @@ class DashBoard extends StatelessWidget {
           backgroundColor: Colors.green,
         ),
         body: Column(
-  children: [
-    Padding(padding: EdgeInsets.only(top: 20)),
-    Text(
-      'Weight',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 18,
-      ),
-    ),
-    Padding(
-      padding: EdgeInsets.only(top: 36.0),
-      child: SizedBox(
-        width: 450,
-        height: 160,
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: Uri.parse(
-              'https://thingspeak.com/channels/2071785/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15',
+          children: [
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Text(
+              'Weight',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
             ),
-          ),
-          initialOptions: InAppWebViewGroupOptions(
-            crossPlatform: InAppWebViewOptions(
-              javaScriptEnabled: true,
+            Padding(
+              padding: EdgeInsets.only(top: 36.0),
+              child: SizedBox(
+                width: 450,
+                height: 200,
+                child: InAppWebView(
+                    initialUrlRequest: URLRequest(
+                      url: Uri.parse(
+                        'https://thingspeak.com/channels/2071785/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15',
+                      ),
+                    ),
+                    initialOptions: InAppWebViewGroupOptions(
+                      crossPlatform: InAppWebViewOptions(
+                        javaScriptEnabled: true,
+                      ),
+                    ),
+                ),
+              ),
             ),
-          ),
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Text(
+              'Distance',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 36.0),
+              child: SizedBox(
+                width: 450,
+                height: 200,
+                child: InAppWebView(
+                  initialUrlRequest: URLRequest(
+                    url: Uri.parse(
+                      'https://thingspeak.com/channels/2071785/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15',
+                    ),
+                  ),
+                  initialOptions: InAppWebViewGroupOptions(
+                    crossPlatform: InAppWebViewOptions(
+                      javaScriptEnabled: true,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
-    ),
-    Padding(padding: EdgeInsets.only(top: 20)),
-    Text(
-      'Distance',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 18,
-      ),
-    ),
-    Padding(
-      padding: EdgeInsets.only(top: 36.0),
-      child: SizedBox(
-        width: 450,
-        height: 160,
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: Uri.parse(
-              'https://thingspeak.com/channels/2071785/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15',
-            ),
-          ),
-          initialOptions: InAppWebViewGroupOptions(
-            crossPlatform: InAppWebViewOptions(
-              javaScriptEnabled: true,
-            ),
-          ),
-        ),
-      ),
-    ),
-  ],
-),
-
       );
 }

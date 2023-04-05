694f663169efd91ecc07e8081d1a270b0d29ca40
Kemila <kemiladabare9@gmail.com>
2023-04-06 05:20:32 +0530

    Minor changes in the dash-board

--- FlutterApp-Digibinn/lib/pages/Dashboard.dart
+++ FlutterApp-Digibinn/lib/pages/Dashboard.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class DashBoard extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) => Scaffold(
         //drawer: NavigationDrawerWidget(),
@@ -12,22 +11,23 @@ class DashBoard extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.green,
         ),
-        body: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 20)),
-            Text(
-              'Weight',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+        body: Center(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 20)),
+              Text(
+                'Weight',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 36.0),
-              child: SizedBox(
-                width: 450,
-                height: 200,
-                child: InAppWebView(
+              Padding(
+                padding: EdgeInsets.only(top: 36.0),
+                child: SizedBox(
+                  width: 450,
+                  height: 200,
+                  child: InAppWebView(
                     initialUrlRequest: URLRequest(
                       url: Uri.parse(
                         'https://thingspeak.com/channels/2071785/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15',
@@ -38,37 +38,38 @@ class DashBoard extends StatelessWidget {
                         javaScriptEnabled: true,
                       ),
                     ),
-                ),
-              ),
-            ),
-            Padding(padding: EdgeInsets.only(top: 20)),
-            Text(
-              'Distance',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 36.0),
-              child: SizedBox(
-                width: 450,
-                height: 200,
-                child: InAppWebView(
-                  initialUrlRequest: URLRequest(
-                    url: Uri.parse(
-                      'https://thingspeak.com/channels/2071785/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15',
-                    ),
-                  ),
-                  initialOptions: InAppWebViewGroupOptions(
-                    crossPlatform: InAppWebViewOptions(
-                      javaScriptEnabled: true,
-                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Padding(padding: EdgeInsets.only(top: 20)),
+              Text(
+                'Distance',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 36.0),
+                child: SizedBox(
+                  width: 450,
+                  height: 200,
+                  child: InAppWebView(
+                    initialUrlRequest: URLRequest(
+                      url: Uri.parse(
+                        'https://thingspeak.com/channels/2071785/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15',
+                      ),
+                    ),
+                    initialOptions: InAppWebViewGroupOptions(
+                      crossPlatform: InAppWebViewOptions(
+                        javaScriptEnabled: true,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       );
 }

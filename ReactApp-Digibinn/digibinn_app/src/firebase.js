7f3af4dd1c955638a751c5e9ccf6ba75bd6648c2
Hasitha Wimalasooriya <57836866+techhasi@users.noreply.github.com>
2023-04-06 10:08:33 +0530

    error fixing

--- ReactApp-Digibinn/digibinn_app/src/firebase.js
+++ ReactApp-Digibinn/digibinn_app/src/firebase.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import {initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCsRBO4p_LIrGv4ePaMpM92T_E3smpOibw",
   authDomain: "digibinn-20537.firebaseapp.com",
